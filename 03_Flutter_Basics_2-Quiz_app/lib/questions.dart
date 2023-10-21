acf437785883f620fb18e207e81f71cda1e9276c
khnychkin_kirill <kirill.khnychkin@gmail.com>
2023-10-22 01:34:16 +0400

    Add questions stateful widget as a placeholder

---
+++ 03_Flutter_Basics_2-Quiz_app/lib/questions.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// TODO: implement widget
+class Questions extends StatefulWidget {
+  const Questions({super.key});
+
+  @override
+  State<Questions> createState() {
+    return _QuestionsState();
+  }
+}
+
+class _QuestionsState extends State<Questions> {
+  @override
+  Widget build(BuildContext context) {
+    return (const Text('questions placeholder'));
+  }
+}
