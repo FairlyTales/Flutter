98e101c5bd6c29ce508e787f5c769616e55eafc1
khnychkin_kirill <kirill.khnychkin@gmail.com>
2023-10-28 21:58:47 +0400

    Add question model and questions array data

---
+++ 03_Flutter_Basics_2-Quiz_app/lib/data/questions.dart
@@ -0,0 +1,34 @@
+import '../models/quiz_question.dart';
+
+const List<QuizQuestion> questions = [
+  QuizQuestion('Question 1', [
+    'Answer 1',
+    'Answer 2',
+    'Answer 3',
+    'Answer 4',
+  ]),
+  QuizQuestion('Question 2', [
+    'Answer 1',
+    'Answer 2',
+    'Answer 3',
+    'Answer 4',
+  ]),
+  QuizQuestion('Question 3', [
+    'Answer 1',
+    'Answer 2',
+    'Answer 3',
+    'Answer 4',
+  ]),
+  QuizQuestion('Question 4', [
+    'Answer 1',
+    'Answer 2',
+    'Answer 3',
+    'Answer 4',
+  ]),
+  QuizQuestion('Question 5', [
+    'Answer 1',
+    'Answer 2',
+    'Answer 3',
+    'Answer 4',
+  ]),
+];
